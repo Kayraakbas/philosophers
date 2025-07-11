0432b46f3de46a3fcf9564ca005c23a6c328277a
Ömer Kayra Akbaş <omakbas@k1m06s04.42istanbul.com.tr>
2025-07-11 22:30:52 +0300

    mutex_array ve philo yapısı oluştuurlmaya başlandı free fonskiyonu yazlacak kod eksik

--- philo.h
+++ philo.h
@@ -7,17 +7,22 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+typedef struct s_resources
+{
+    int time_to_think; 
+    int time_to_eat;
+    int time_to_sleep;
+    int each_philos_must_eat;
+    pthread_mutex_t *locks;
+} t_resources;
+
 
 typedef struct s_philo
 {
+    int id;
     pthread_t thread;
-
-    int time_to_eat;
-    int time_to_sleep;
-    int notepme;
-    pthread_mutex_t left;
-    pthread_mutex_t right;
-
+    pthread_mutex_t *left;
+    pthread_mutex_t *right;
 } t_philo;
 
 #endif
