6af2b3e62fd32930a545aae1c101581821895212
Ömer Kayra Akbaş <omakbas@k1m09s08.42istanbul.com.tr>
2025-07-20 13:59:29 +0300

    leak fix

--- philo.c
+++ philo.c
@@ -3,21 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kayraakbas <kayraakbas@student.42.fr>      +#+  +:+       +#+        */
+/*   By: omakbas <omakbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 00:00:00 by omakbas           #+#    #+#             */
-/*   Updated: 2025/07/19 17:13:28 by kayraakbas       ###   ########.fr       */
+/*   Updated: 2025/07/20 13:58:58 by omakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void handle_1_philo(t_philo philo, t_resources resources)
+static void	join_philosopher_threads(t_resources resources, t_philo *philo_arr)
 {
-	safe_sleep(resources.time_to_die);
-	safe_print(&philo, "died");
-	safe_free_all(resources);
-	exit(1);
+	int	i;
+
+	if (resources.num_of_philos == 1)
+		pthread_detach(philo_arr[0].thread);
+	else
+	{
+		i = 0;
+		while (i < resources.num_of_philos)
+		{
+			pthread_join(philo_arr[i].thread, NULL);
+			i++;
+		}
+	}
 }
 
 int	main(int argc, char **argv)
@@ -26,7 +35,6 @@ int	main(int argc, char **argv)
 	t_resources		resources;
 	t_philo			*philo_arr;
 	pthread_t		monitor_thread;
-	int				i;
 
 	if (check_arguments(argc, argv) == false)
 		exit(1);
@@ -35,16 +43,8 @@ int	main(int argc, char **argv)
 	philo_arr = create_philo_arr(resources.num_of_philos, &resources);
 	resources.philos = philo_arr;
 	pthread_create(&monitor_thread, NULL, death_monitor, &resources);
-	if (resources.num_of_philos == 1)
-		handle_1_philo(philo_arr[0], resources);
-	i = 0;
-	while (i < resources.num_of_philos)
-	{
-		pthread_join(philo_arr[i].thread, NULL);
-		i++;
-	}
+	join_philosopher_threads(resources, philo_arr);
 	pthread_join(monitor_thread, NULL);
-	resources.simulation_ended = true;
 	safe_free_all(resources);
 	return (0);
 }
